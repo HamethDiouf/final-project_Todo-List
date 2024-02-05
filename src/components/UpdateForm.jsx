c76f740cb34004f9e4a6cc30a5f5e6e17f0fbadd
HamethDiouf <dioufameth49@gmail.com>
2024-02-05 15:37:12 +0000

    update todo

--- src/components/UpdateForm.jsx
+++ src/components/UpdateForm.jsx
@@ -5,7 +5,7 @@ const UpdateForm = ({updateData, changeTask, updateTask, cancelTask}) => {
        <div className='row'>
          <div className='col'>
            <input id="updateTask"
-           placeholder="Update task"
+           placeholder="Update your task"
              value={ updateData && updateData.title }
               onChange={ (e) => changeTask(e)}
              className='form-control form-control-lg'
